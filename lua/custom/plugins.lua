2f4a329b0fce899d7f80b5a6e6b19d0f8c02247b
Jewin Varghese <86153268+JewinV@users.noreply.github.com>
2023-07-18 10:34:47 +0530

    adding copilot and rainbow brackets.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -22,5 +22,79 @@ local plugins = {
       vim.g.rustfmt_autosave = 1
     end
   },
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    event = "BufReadPost",
+    config = function()
+      local rainbow_delimiters = require "rainbow-delimiters"
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      require "custom.configs.copilot"
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-a>",
+            accept_word = "<C-w>",
+            accept_line = "<C-s>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<M-c>",
+          },
+        },
+        panel = {
+          enabled = false,
+        },
+        server_opts_overrides = {
+          trace = "verbose",
+          settings = {
+            advanced = {
+              listCount = 3,
+              inlineSuggestCount = 3,
+            },
+          },
+        },
+      }
+    end,
+  },
 }
 return plugins
