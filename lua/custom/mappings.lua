f90ccaef48310d27c41bfc57dcce03994bf1f57b
Jewin Varghese <86153268+JewinV@users.noreply.github.com>
2023-07-27 17:57:17 +0530

    toggling copilot with <F3> and <F4>

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,7 +5,9 @@ M.abc = {
   n = {
      ["<C-u>"] = {"<C-u>zz", "Scroll Up"},
      ["<C-d>"] = {"<C-d>zz", "Scroll Down"},
-  }
+     ["<F3>"] = {":Copilot enable<CR>", "Enabling Copilot"},
+     ["<F4>"] = {":Copilot disable<CR>", "Disabling Copilot"}
+  },
 }
 
 return M
